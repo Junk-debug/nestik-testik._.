b35d73b3eea444984e152a94e539b0a03a4fda92
Junk-debug <maks.naumovich@gmail.com>
2025-02-14 14:11:51 +0100

    feat: handle favicon requests with a 204 response

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { enableApiDocs } from './utils/docs';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   enableApiDocs('api/docs', app);
+
+  app.use('/favicon.ico', (_: Request, res: Response) => res.status(204).end());
 
   await app.listen(process.env.PORT ?? 3000);
 }
